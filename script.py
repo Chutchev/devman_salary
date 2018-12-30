4c8a199600efd53a109585edb61519919ba86c06
Chutchev <iChutchev98@gmail.com>
2018-12-30 22:40:35 +0500

    вывод средней зарплаты , кол-во вакансий/языки

--- script.py
+++ script.py
@@ -3,23 +3,25 @@ import datetime
 from dateutil import parser
 
 
-def get_predict_rub_salary(language):
+def get_predict_rub_salary(language, pages:int):
     url = "https://api.hh.ru/vacancies"
-    print(language)
-    parameters = {'text': f'программист {language}'}
-    response = requests.get(url, params=parameters)
-    for vacancy in response.json()['items']:
-        if vacancy['salary'] is not None:
-            if vacancy['salary']['currency'] == 'RUR':
-                if vacancy['salary']['to'] is None:
-                    print(f"\t{vacancy['salary']['from'] * 1.2}")
-                elif vacancy['salary']['from'] is None:
-                    print(f"\t{vacancy['salary']['to'] * 0.8}")
-                elif (vacancy['salary']['from'], vacancy['salary']['to']) is not None:
-                    salary = (vacancy['salary']['to'] + vacancy['salary']['from']) / 2
-                    print(f"\t{salary}")
-            else:
-                print(f"\t{None}")
+    salaries = []
+    for page in range(pages):
+        print(language, page)
+        parameters = {'text': f'программист {language}',
+                      'per_page': 100, 'page': page}
+        response = requests.get(url, params=parameters)
+        for vacancy in response.json()['items']:
+            if vacancy['salary'] is not None:
+                if vacancy['salary']['currency'] == 'RUR':
+                    if vacancy['salary']['to'] is None:
+                        salaries.append(vacancy['salary']['from'] * 1.2)
+                    elif vacancy['salary']['from'] is None:
+                        salaries.append(vacancy['salary']['to'] * 0.8)
+                    elif (vacancy['salary']['from'], vacancy['salary']['to']) is not None:
+                        salaries.append((vacancy['salary']['to'] + vacancy['salary']['from']) / 2)
+    averages = int(sum(salaries)/(len(salaries)))
+    return averages, len(salaries)
 
 
 def get_amount_vacancies(languages):
@@ -27,33 +29,31 @@ def get_amount_vacancies(languages):
     url = "https://api.hh.ru/vacancies"
     for language in languages:
         parameters = {'text': f'программист {language}'}
-        response = requests.get(url, params=parameters)
-        if response.json()['found'] > 100:
-            need_vacancies[language] = response.json()['found']
-    return need_vacancies
-
-
-def get_programmer_vacancies(languages):
-    url = "https://api.hh.ru/vacancies"
-    for language in languages:
-        parameters = {'text': f'программист {language}'}
         print(language)
-        response = requests.get(url, params=parameters)
-        for vacancy in response.json()['items']:
-            vacancy_datetime = parser.parse(vacancy['published_at'])
-            today = datetime.date.today()
-            vacancy_date = datetime.date(vacancy_datetime.year, vacancy_datetime.month, vacancy_datetime.day)
-            if vacancy['area']['name'] == 'Москва' and (today-vacancy_date).days < 30:
-                print(f'\t{vacancy}')
+        pages = requests.get(url, params=parameters).json()['pages']
+        for page in range(pages):
+            parameters = {'text': f'программист {language}',
+                           'per_page': 5, 'page': page}
+            response = requests.get(url, params=parameters)
+            for vacancy in response.json()['items']:
+                vacancy_datetime = parser.parse(vacancy['published_at'])
+                today = datetime.date.today()
+                vacancy_date = datetime.date(vacancy_datetime.year, vacancy_datetime.month, vacancy_datetime.day)
+                if response.json()['found'] > 100 and vacancy['area']['name'] == 'Москва' \
+                        and (today-vacancy_date).days < 30:
+                    need_vacancies[language] = [response.json()['found'], response.json()['pages']]
+    return need_vacancies
 
 
 def main():
     languages = ['JavaScipt', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'GO']
-    #get_programmer_vacancies(languages)
-    vacancies, count = get_amount_vacancies(languages)
+    vacancies = get_amount_vacancies(languages)
     print(vacancies)
     for language in vacancies.keys():
-        get_predict_rub_salary(language)
+        average, processed = get_predict_rub_salary(language, vacancies[language][1])
+        info_for_language = {'found': vacancies[language][0], 'vacancies_processed': processed, 'average_salary': average}
+        vacancies[language] = info_for_language
+    print(vacancies)
 
 
 if __name__ == "__main__":
